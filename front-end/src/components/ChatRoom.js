d33ec2991634b63fc22ced57b769a4151a4d746b
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-27 13:02:27 -0600

    Update ChatRoom.js

--- front-end/src/components/ChatRoom.js
+++ front-end/src/components/ChatRoom.js
@@ -1,1 +1,26 @@
 // src/components/ChatRoom.js
+
+import React, { useState } from 'react';
+import Message from './Message';
+import SendMessageForm from './SendMessageForm';
+
+const ChatRoom = () => {
+  const [messages, setMessages] = useState([]);
+
+  const addMessage = (text) => {
+    setMessages([...messages, { text }]);
+  };
+
+  return (
+    <div className="chat-room">
+      <div className="message-list">
+        {messages.map((message, index) => (
+          <Message key={index} text={message.text} />
+        ))}
+      </div>
+      <SendMessageForm addMessage={addMessage} />
+    </div>
+  );
+};
+
+export default ChatRoom;
