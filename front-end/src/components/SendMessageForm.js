f3489b3f0e866a11c4318bad6fb81533f4d729b4
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-27 13:04:53 -0600

    Update SendMessageForm.js

--- front-end/src/components/SendMessageForm.js
+++ front-end/src/components/SendMessageForm.js
@@ -1,1 +1,27 @@
 // src/components/SendMessageForm.js
+
+import React, { useState } from 'react';
+
+const SendMessageForm = ({ addMessage }) => {
+  const [inputText, setInputText] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addMessage(inputText);
+    setInputText('');
+  };
+
+  return (
+    <form className="send-message-form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Type your message..."
+        value={inputText}
+        onChange={(e) => setInputText(e.target.value)}
+      />
+      <button type="submit">Send</button>
+    </form>
+  );
+};
+
+export default SendMessageForm;
