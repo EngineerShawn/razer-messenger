af5b155f72ab63f43d30dea69e010d1b1ae029f4
Shawn Patterson <147225693+EngineerShawn@users.noreply.github.com>
2024-02-28 00:26:24 -0600

    updates

--- back-end/models/User.js
+++ back-end/models/User.js
@@ -1,30 +1,30 @@
 // backend/models/User.js
 
-const pool = require('../config/db');
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
-
+const pool = require("../config/db");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 class User {
   async createUser(username, password) {
-    const query = 'INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *';
+    const query =
+      "INSERT INTO users (username, password) VALUES ($1, $2) RETURNING *";
     const values = [username, password];
     try {
       const { rows } = await pool.query(query, values);
       return rows[0];
     } catch (err) {
-      console.error('Error creating user:', err);
+      console.error("Error creating user:", err);
     }
   }
 
   async findUserByUsername(username) {
-    const query = 'SELECT * FROM users WHERE username = $1';
+    const query = "SELECT * FROM users WHERE username = $1";
     const values = [username];
     try {
       const { rows } = await pool.query(query, values);
       return rows[0];
     } catch (err) {
-      console.error('Error finding user by username:', err);
+      console.error("Error finding user by username:", err);
     }
   }
 }
